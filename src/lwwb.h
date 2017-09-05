2da08cec0faa38037430b0355a0198de37f506b7
Jarred Capellman <jcapellman@hotmail.com>
2017-09-04 20:45:39 -0500

    Added: Main Header

---
+++ src/lwwb.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/FL_Input.h>
+#include <FL/Fl_Multi_Browser.H>
+
+#pragma comment(lib, "fltk.lib")
+
+using namespace std;
