bbd730bc3f64c6bbb7c2f03d0af8d501c656da8a
Jarred Capellman <jcapellman@hotmail.com>
2017-09-04 20:44:05 -0500

    Added: URL and Content UI elements

--- src/lwwb.cpp
+++ src/lwwb.cpp
@@ -1,16 +1,15 @@
-#include <FL/Fl.H>
-#include <FL/Fl_Window.H>
-
-#pragma comment(lib, "fltk.lib")
-
-using namespace std;
+#include "lwwb.h"
 
 int main()
 {
 	Fl_Window win(800, 600, "lwwb");
 	
 	win.begin();
-	
+
+	Fl_Input * inputURL = new Fl_Input(5, 5, 790, 30);
+
+	Fl_Multi_Browser browserContent(5, 45, 790, 550);
+
 	win.end();
 	
 	win.show();
